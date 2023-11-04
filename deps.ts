2161013cd53986aa80eaee76c1edd2b252910082
daiki.nakajima <daiki.nakajima531@gmail.com>
2023-11-04 21:15:37 +0900

    initial commit

---
+++ deps.ts
@@ -0,0 +1,6 @@
+import "npm:zenn-cli@latest";
+import "npm:textlint@latest";
+import "npm:textlint-rule-preset-ja-spacing@latest";
+import "npm:textlint-rule-preset-jtf-style@latest";
+import "npm:textlint-rule-spellcheck-tech-word@latest";
+import "npm:textlint-rule-preset-ja-technical-writing@latest";
